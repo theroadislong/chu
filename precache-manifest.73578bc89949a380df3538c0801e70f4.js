7cad0a97848e3e64172e974202764f70ca67120b
Artem <dunaev.aa@yandex.ru>
2019-03-25 22:19:33 +0500

    Updates

---
+++ precache-manifest.73578bc89949a380df3538c0801e70f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d15a138b00a51e5d3b4",
+    "url": "/chu/static/js/runtime~main.4d15a138.js"
+  },
+  {
+    "revision": "bcf18c30dafe72edaa96",
+    "url": "/chu/static/js/main.bcf18c30.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/chu/static/js/2.38e92809.chunk.js"
+  },
+  {
+    "revision": "bcf18c30dafe72edaa96",
+    "url": "/chu/static/css/main.05954d22.chunk.css"
+  },
+  {
+    "revision": "9f73e4d2eb03bc980c5f2431222f0906",
+    "url": "/chu/index.html"
+  }
+];
